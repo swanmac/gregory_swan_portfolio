70f889bf38212184010b2f4139b17dd7bc21444b
Greg Swan <gmswan99@gmail.com>
2022-12-09 18:53:41 -0500

    added images to projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -5,11 +5,14 @@ export default function Projects () {
     return(
         <div className="projects">
 
-        <Link to="project1"><h3>Project1</h3></Link>
+        <Link to="project1"><h3>Project-1 Tic Tac Toe</h3></Link>
+        <img src="https://sunbizlocal.com/tic.png"></img>
 
-        <Link to="project2"><h3>Project2</h3></Link>
+        <Link to="project2"><h3>Project-2 Trivia Junction</h3></Link>
+        <img src="https://sunbizlocal.com/triv.png"></img>
 
-        <Link to="project3"><h3>Project3</h3></Link>
+        <Link to="project3"><h3>Project-3 The Cocktail Selector</h3></Link>
+        <img src="https://sunbizlocal.com/coct.png"></img>
 
 </div>
  )           
