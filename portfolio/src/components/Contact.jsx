db99e0633ab6e017b71c4cd02ec70cb8ce6fdfad
Greg Swan <gmswan99@gmail.com>
2023-01-09 12:08:32 -0500

    update resume

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact () {
             <h2>Contact</h2>
             <h3>Phone: 941-268-1926</h3>
             <h3><a href="mailto:gmswan99@gmail.com">gmswan99@gmail.com</a></h3>  
-            <h3> <a href="https://www.linkedin.com/in/gregoryswan/" target=" _blank" rel="noopener noreferrer">LinkedIn</a></h3>  
+            {/* <h3> <a href="https://www.linkedin.com/in/gregoryswan/" target=" _blank" rel="noopener noreferrer">LinkedIn</a></h3>   */}
             
          
         
