b61a0f294f43bc4b8008dfb73d8169cc77ffed5d
Greg Swan <gmswan99@gmail.com>
2023-06-05 20:29:57 -0400

    add project

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -17,10 +17,10 @@ export default function Projects () {
         <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>A ten question true/false trivia game created with JavaScript, HTML, and CSS connected to a third party API.</p>
         <Link to="/project2"><img className='mx-auto flex items-center pt-4 pb-5 w-300 h-220'  src="https://sunbizlocal.com/triva.png"></img></Link></div>
 
-        {/* <div className="project p1"> 
+        <div className="project p1"> 
         <Link to="/project3"><h3 className='text-2xl'>The Cocktail Selector</h3></Link>
-        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>An application created using the React framework, JavaScript and CSS, connected to a third party API.</p>
-        <Link to="/project3"><img className='mx-auto flex items-center pt-4 pb-5 w-300 h-220'  src="https://sunbizlocal.com/coct.png"></img></Link></div> */}
+        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>An application created using the React framework, JavaScript and Tailwind CSS, connected to a third party API.</p>
+        <Link to="/project3"><img className='mx-auto flex items-center pt-4 pb-5 w-300 h-220'  src="https://sunbizlocal.com/coct.png"></img></Link></div>
 
         <div className="project p1"> 
         <Link to="/project4"><h3 className='text-2xl'>Metal Geek</h3></Link>
