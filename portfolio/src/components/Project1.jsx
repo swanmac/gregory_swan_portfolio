4731766873cba8ed2f0295fd05d1e255e1383f79
Greg Swan <gmswan99@gmail.com>
2023-11-11 12:36:09 -0500

    update project1

--- portfolio/src/components/Project1.jsx
+++ portfolio/src/components/Project1.jsx
@@ -11,7 +11,7 @@ export default function Project1 () {
           <h2 className='text-2xl text-[#4CAF50]'>Card Deck</h2>
             <img className='mx-auto flex items-center pt-4 w-300px h-220px' src="https://sunbizlocal.com/phone2.png" alt="project 1" ></img>
             <p className='md:text-xl max-w-lg mx-auto pt-5 pb-5'>
-            An IOS card game built with SwiftUI.</p>
+            An IOS application Card Game. The application was built with Swift using the SwiftUI.</p>
             <h3 className='text-2xl text-[#4CAF50] pt-3 pb-3'>Game Overview</h3>
             
                 <p className='md:text-xl max-w-lg mx-auto pb-5'>A user will click on the deal button to deal the cards.
