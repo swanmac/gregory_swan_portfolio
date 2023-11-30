981eb3033f2ccb20fbef14dae077c31d4d19fc52
Greg Swan <gmswan99@gmail.com>
2023-11-29 20:18:28 -0500

    update projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -14,17 +14,17 @@ export default function Projects () {
 
         <div className="project p2"> 
         <Link to="/project2"><h3 className='text-2xl'>Trivia Junction</h3></Link>
-        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>A true/false trivia game created with JavaScript, HTML5, and CSS connected to a third party API.</p>
+        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>A true/false trivia game created with JavaScript, HTML5, and CSS with a third party API.</p>
         <Link to="/project2"><img className='mx-auto flex items-center pt-4 pb-5 w-300 h-220'  src="https://sunbizlocal.com/triva.png" alt="project 2" ></img></Link></div>
 
         <div className="project p1"> 
         <Link to="/project3"><h3 className='text-2xl'>The Cocktail Selector</h3></Link>
-        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>An application created using the React framework, JavaScript, HTML5 and CSS, connected to a third party API.</p>
+        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>An application developed using the React framework, JavaScript, HTML5 and CSS, connected to a third party API.</p>
         <Link to="/project3"><img className='mx-auto flex items-center pt-4 pb-5 w-300 h-220'  src="https://sunbizlocal.com/coc.png" alt="project 3" ></img></Link></div>
 
         <div className="project p1"> 
         <Link to="/project4"><h3 className='text-2xl'>Metal Geek</h3></Link>
-        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>A full-stack application with React (front end) and Django (back end) built with a PostgreSQL database. Styled with Tailwind CSS.</p>
+        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>A full-stack application with React (front end) and Django (back end) using a PostgreSQL database. Styled with Tailwind CSS.</p>
         <Link to="/project4"><img className='mx-auto flex items-center pt-4 pb-5 w-300 h-220' src="https://sunbizlocal.com/meta.png" alt="project 4" ></img></Link></div>
 
         <br />
