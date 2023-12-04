b84836b2a9f8ed6758d94e829d3463f31958ee18
Greg Swan <gmswan99@gmail.com>
2023-12-03 19:39:08 -0500

    update projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects () {
 
         <div className="project p2"> 
         <Link to="/project2"><h3 className='text-2xl'>Trivia Junction</h3></Link>
-        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>A true/false trivia game created with JavaScript, HTML5, and CSS with a third party API.</p>
+        <p className='md:text-xl max-w-lg mx-auto pb-5 px-10'>A true/false trivia game created with JavaScript, HTML5, and CSS3 with a third party API.</p>
         <Link to="/project2"><img className='mx-auto flex items-center pt-4 pb-5 w-300 h-220'  src="https://sunbizlocal.com/triva.png" alt="project 2" ></img></Link></div>
 
         <div className="project p1"> 
